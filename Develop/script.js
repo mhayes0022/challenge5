0778065092b5ea29c4ba38628a76ed23fae4d659
MHayes <mhayes0022@gmail.com>
2024-02-29 21:40:28 -0600

    another

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,25 @@
+const apiUrl = "path/to/dayjs/dayjs.min.js";
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-//$(function () {
+//Do I even need the variables below?
+//let hourNine = document.getElementById('hour-9');
+// let hourTen = document.getElementById('hour-10');
+// let hourEleven = document.getElementById('hour-11');
+// let hourTwelve = document.getElementById('hour-12');
+// let hourThirteen = document.getElementById('hour-13');
+// let hourFourteen = document.getElementById('hour-14');
+// let hourFifteen = document.getElementById('hour-15');
+// let hourSixteen = document.getElementById('hour-16');
+// let hourSeventeen = document.getElementById('hour-17');
+//above: wont let me create variable with numbers as the name
+
+
+$(function () {
+  let currentDay = document.getElementById('currentDay');
+
+  currentDay = dayjs().format('MMM D, YYYY');
+  $('#currentDay').text(currentDay);
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -14,45 +32,57 @@
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+
+  //Could I just name all HTML ids as nums, have checkHours read the value?
+
+  //plannerHour();
+  // let hourNine = [document.getElementById('hour-9'), 9];
+  // let hourTen = document.getElementById('hour-10');
+  // let hourEleven = document.getElementById('hour-11');
+  // let hourTwelve = document.getElementById('hour-12');
+  // let hourThirteen = document.getElementById('hour-13');
+  // let hourFourteen = document.getElementById('hour-14');
+  // let hourFifteen = document.getElementById('hour-15');
+  // let hourSixteen = document.getElementById('hour-16');
+  // let hourSeventeen = document.getElementById('hour-17');
+
+  // plannerHour = [
+  //   hourNine = 9,
+  //   hourTen = 10,
+  //   hourEleven = 11,
+  //   hourTwelve = 12,
+  //   hourThirteen = 13,
+  //   hourFourteen = 14,
+  //   hourFifteen = 15,
+  //   hourSixteen = 16,
+  //   hourSeventeen = 17
+  // ]
+
+
+  function checkHour() {
+    currentHour = dayjs().hour();
+    if (currentHour > plannerHour[index.value]) {
+      $('#hour-12').addClass('past');
+    }
+    if (currentHour < plannerHour[index.value]) {
+      $('#hour-12').addClass('future');
+    }
+    if (currentHour == plannerHour[index.value]) {
+      $('#hour-12').addClass('present');
+    }
+  }
+
+
+
+  checkHour();
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-//});
 
-//Beginnings of API request
-const apiUrl = "path/to/dayjs/dayjs.min.js";
-//dateOutput;
-let currentDay = document.getElementById('currentDay');
+});
 
-currentDay = dayjs().format('MMM D, YYYY');
-$('#currentDay').text(currentDay);
+//document.getElementById("myNumber").value = "16";
 
-//function test() {
-//fetch('https://cdn.jsdelivr.net/npm/dayjs@1/dayjs.min.js')
-//.then(response => {
-//  if (!response.ok) {
-//    throw new Error('Something went wrong with network response');
-//  }
-//  return response.json();
-//})
-//.then(data => {
-  //.textContent = JSON.stringify()
-//  console.log(data);
-//})
-// .catch(error => {
-//   console.error('Error', error);
-// });
-//}
-// test();
-//Ok. so the above needs to be more specific. Right now it's calling all of the API I think.
-
-
-// .catch(error => {
-//   console.error('Error:', error);
-// });
 //To start:
-//finish API request, get it to display at top of page
 //$('#myElement').addClass('active'); remember you can use jQuery now to select & change the class
